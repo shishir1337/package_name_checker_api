7d817d4653ff36e50ad1d3cbcf5abbc50387f80a
Md. Shishir Ahmed <shishirdevilzz@gmail.com>
2024-07-13 03:32:31 +0600

    Update app.py

--- app.py
+++ app.py
@@ -66,5 +66,5 @@ def get_app_details_api():
     else:
         return jsonify({'message': 'Package name is unique!'})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
